49dd2a50b50b558164cb135105b38700fc945983
RonaldMSn <rmaldonado@soaint.com>
2021-12-20 17:37:34 -0500

    Adding tables of app

---
+++ fhvts/src/main/java/com/fhvts/domain/VoteId.java
@@ -0,0 +1,35 @@
+package com.fhvts.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class VoteId implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5150909884015869992L;
+	private User user;
+	private Feature feature;
+	
+	@ManyToOne
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@ManyToOne
+	public Feature getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+	
+	
+}
---
+++ fhvts/src/main/java/com/fhvts/domain/Votes.java
@@ -0,0 +1,30 @@
+package com.fhvts.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+@Embeddable
+public class Votes {
+	
+	
+	private VoteId pk;
+	private boolean upvote;
+	
+	@EmbeddedId
+	public VoteId getPk() {
+		return pk;
+	}
+	public void setPk(VoteId pk) {
+		this.pk = pk;
+	}
+	public boolean isUpvote() {
+		return upvote;
+	}
+	public void setUpvote(boolean upvote) {
+		this.upvote = upvote;
+	}
+	
+	
+}
