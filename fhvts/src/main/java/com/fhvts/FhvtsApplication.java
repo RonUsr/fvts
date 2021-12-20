397893d92eed6967e00a9f3a570cd69acb91a995
RonaldMSn <rmaldonado@soaint.com>
2021-12-20 16:36:51 -0500

    Initial Check-IN

---
+++ fhvts/src/main/java/com/fhvts/FhvtsApplication.java
@@ -0,0 +1,13 @@
+package com.fhvts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhvtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhvtsApplication.class, args);
+	}
+
+}
---
+++ fhvts/src/test/java/com/fhvts/FhvtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhvts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhvtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
