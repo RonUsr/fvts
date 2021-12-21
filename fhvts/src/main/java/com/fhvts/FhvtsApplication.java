56b790c25a904684c1f610daa0cce952c7131ed5
RonaldMSn <rmaldonado@soaint.com>
2021-12-20 19:42:30 -0500

    Adding fst controller

--- fhvts/src/main/java/com/fhvts/FhvtsApplication.java
+++ fhvts/src/main/java/com/fhvts/FhvtsApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FhvtsApplication {
-
+	
+	//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
 	public static void main(String[] args) {
 		SpringApplication.run(FhvtsApplication.class, args);
 	}
